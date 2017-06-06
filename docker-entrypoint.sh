12ea014d134e0de0e3377b0b37833f495dbc70bc
thb <thb.krkr@gmail.com>
2017-06-07 01:02:38 +0200

    Support multiple registries and ssh key creds
      - Support multiple registries in REGISTRY_AUTH (base64
        ~/.docker/config.json)
      - Support ssh key inlined in the variable in base64
        ex: SEED_CREDS_ID=ci-bitbucket.id_rsa...:LS0tLS1CR

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -eu
 
-if [[ "${REGISTRY_URL:-}" != "" ]]; then
+if [[ "${REGISTRY_AUTH:-}" != "" ]]; then
   mkdir -p ~/.docker
-  echo '{
-      "auths": {
-        "'$REGISTRY_URL'": {
-            "auth": "'$REGISTRY_AUTH'"
-        }
-      }
-    }' > ~/.docker/config.json
+  base64 -d <<< $REGISTRY_AUTH > ~/.docker/config.json
+fi
+
+if [[ "${SEED_CREDS:-}" != "" ]]; then
+  mkdir -p /usr/share/jenkins/keys
+  export SEED_CREDS_ID=$(cut -d ':' -f1 <<< $SEED_CREDS)
+  cut -d ':' -f2 <<< $SEED_CREDS | base64 -d > /usr/share/jenkins/keys/$SEED_CREDS_ID
 fi
 
 exec /bin/tini -- /usr/local/bin/jenkins.sh
