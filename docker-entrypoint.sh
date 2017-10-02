3ec84e272d278844a22f9ed1803d018562a0743b
thb <thb.krkr@gmail.com>
2017-10-02 13:05:22 +0200

    chmod 600 ssh key and docker config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,12 +3,14 @@
 if [[ "${REGISTRY_AUTH:-}" != "" ]]; then
   mkdir -p ~/.docker
   base64 -d <<< $REGISTRY_AUTH > ~/.docker/config.json
+  chmod 600 ~/.docker/config.json
 fi
 
 if [[ "${SEED_CREDS:-}" != "" ]]; then
   mkdir -p /usr/share/jenkins/keys
   export SEED_CREDS_ID=$(cut -d ':' -f1 <<< $SEED_CREDS)
   cut -d ':' -f2 <<< $SEED_CREDS | base64 -d > /usr/share/jenkins/keys/$SEED_CREDS_ID
+  chmod 600 /usr/share/jenkins/keys/$SEED_CREDS_ID
 fi
 
 exec /bin/tini -- /usr/local/bin/jenkins.sh
