505e1781ad39397dcec04b2ebefc905ca83609cb
thb <thb.krkr@gmail.com>
2017-12-16 18:04:20 +0100

    Add configure-jnlp-agent-protocols groovy init script

---
+++ init.groovy.d/10-configure-jnlp-agent-protocols.groovy
@@ -0,0 +1,22 @@
+/*
+ * Disable all JNLP protocols except for JNLP4.  JNLP4 is the most secure agent
+ * protocol because it is using standard TLS.
+ */
+import jenkins.model.Jenkins
+
+Jenkins j = Jenkins.instance
+
+if(!j.isQuietingDown()) {
+    Set<String> agentProtocolsList = ['JNLP4-connect', 'Ping']
+    if(!j.getAgentProtocols().equals(agentProtocolsList)) {
+        j.setAgentProtocols(agentProtocolsList)
+        println "Agent Protocols have changed.  Setting: ${agentProtocolsList}"
+        j.save()
+    }
+    else {
+        println "Nothing changed.  Agent Protocols already configured: ${j.getAgentProtocols()}"
+    }
+}
+else {
+    println 'Shutdown mode enabled.  Configure Agent Protocols SKIPPED.'
+}
